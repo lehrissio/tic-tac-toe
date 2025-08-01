025ada48228e71d58789ca5c8a5c2527dd43fe3b
Leticia Rissio Silva <103052994+lehrissio@users.noreply.github.com>
2025-07-31 22:15:12 -0300

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import JogoDaVelha from './components/JogoDaVelha/JogaDaVelha';
+
+function App() {
+  return (
+    <div>
+      <JogoDaVelha/>
+    </div>
+  );
+}
+
+export default App;
